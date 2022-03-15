d0efc799e352a04ed5d76c2ffb9fb7d380c5bf29
OSJ1993 <90177830+OSJ1993@users.noreply.github.com>
2022-03-15 15:37:48 +0900

    20220315

--- RunGame/Assets/Scripts/MobBase.cs
+++ RunGame/Assets/Scripts/MobBase.cs
@@ -6,16 +6,26 @@ public class MobBase : MonoBehaviour
 {
 
     public float mobSpeed = 0;
+    public Vector2 StartCoroutine;
 
-    void Start()
+    //MobPrefabs이 활성화 되면 우측에서 시작해서 화면 좌측을 넘어가면 다시 비활성화. / 22.03.15 by승주.
+    //OnEnable은 오브젝트가 활성화 될 때 실행. /22.03.15 by승주
+    private void OnEnable()
     {
-
+        transform.position = StartCoroutine;
     }
 
 
     void Update()
     {
         //움직임 /22.03.08 by 승주
-        transform.Translate(Vector2.left * Time.deltaTime);
+        transform.Translate(Vector2.left * Time.deltaTime * mobSpeed);
+
+        //오브젝트의 x좌표가 -6보다 작아지면 
+        if (transform.position.x < -6)
+        {
+            //오브젝트 비활성화. /22.03.15 by승주
+            gameObject.SetActive(false);
+        }
     }
 }
--- RunGame/Assets/Scripts/RespawnManager.cs
+++ RunGame/Assets/Scripts/RespawnManager.cs
@@ -17,17 +17,51 @@ public class RespawnManager : MonoBehaviour
         //반복문과 CreatObj 매소드를 사용해서 Mobs배열에 PreFab을 원하는 수만큼 생성 후 /22.03.08 by 승주
         for (int i = 0; i < Mobs.Length; i++)
         {
-            // MobPool List에 추가.
-            MobPool.Add(CreatObj(Mobs[i], transform));
+            for (int q = 0; q < objCnt; q++)
+            {
+
+                // MobPool List에 추가.
+                MobPool.Add(CreatObj(Mobs[i], transform));
+            }
         }
     }
 
 
+    private void Start()
+    {
+        StartCoroutine(CreateMob());
+    }
+
+
     //코르틴을 사용해서 정해진 시간마다 리스트에 장애물을 한개를 랜덤으로 뽑아 활성화. /22.03.08 by 승주
-    //Random.Range(최소, 최대); 최소~최대 사이의 임의 수를 반환. /22.03.08 by 승주
     IEnumerator CreateMob()
     {
-        while(true)
+        // while 으로 반복 실행. /22.03.15 by승주
+        while (true)
+        {
+            //Random.Range(최소, 최대); 최소~최대 사이의 임의 수를 활성화. /22.03.08 by 승주
+            MobPool[Random.Range(0, MobPool.Count)].SetActive(true);
+
+            //WaitForSeconds는 괄호 안에 숫자 만큼 기다렸다가 실행. /22.03.15 by승주
+            yield return new WaitForSeconds(Random.Range(1f, 3f));
+        }
+    }
+
+    //장애물을 활성화 시킬 때 List에서 랜덤으로 실행 시키기 때문에 이미 활성화 되어있는 오브젝트가 다시 활성화 되므로 활성화 되어있지 않는 오브젝트를 찾아서 활성화 시켜주는 함수.  /22.03.15 by 승주
+    int DeactiveMob()
+    {
+        List<int> num = new List<int>();
+
+        //MobPool의 크기만큼 반복하면서 비활성화 되어있는 찾아 순번을 List에 추가
+        for(int i = 0; i < MobPool.Count; i++)
+        {
+            if (!MobPool[i].activeSelf)
+                num.Add(i);
+        }
+        int x = 0;
+        
+       //x=num
+            return;
     }
 
     //게임 오브젝트를 반환형으로 같는 CreateObj로 만들어 주기. /22.03.08 by 승주
